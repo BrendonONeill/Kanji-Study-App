f68cc673dce3c7c1ea4dd576577bba95295fc2b9
Brendon O'Neill <35762480+BrendonONeill@users.noreply.github.com>
2023-04-24 10:41:22 +0100

    Update Pages.tsx

--- src/ts/Pages/Pages.tsx
+++ src/ts/Pages/Pages.tsx
@@ -14,7 +14,7 @@ function Pages() {
   if(GlobalItems?.data.length === 0)
   {
     useEffect(() => {
-    fetch(`https://nodeproxy-production.up.railway.app/kanji/`)
+    fetch(`https://concerned-shirt-dog.cyclic.app/`)
     .then(response => response.json())
     .then(data => GlobalItems?.setData(data.record))
     .catch(error => console.log(error));

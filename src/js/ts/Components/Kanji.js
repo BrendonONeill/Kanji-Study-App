df0cf4e20c5ac74aff1162f0d32f7dd8af36fce8
Brendon O'Neill <35762480+BrendonONeill@users.noreply.github.com>
2023-08-08 10:29:38 +0100

    Update Kanji.js

--- src/js/ts/Components/Kanji.js
+++ src/js/ts/Components/Kanji.js
@@ -4,7 +4,7 @@ import { useContext } from "react";
 const Kanji = () => {
     const GlobalItems = useContext(GlobalContext);
     const handleKeyPress = (event, index) => {
-        if (event.which === 13 || event.which === 9) {
+        if (event.which === 13 || event.which === 9 || event.which === 229) {
             let inputWord = event.target.value.trim();
             let test = inputWord.toLowerCase();
             if (GlobalItems?.gameCards[index].romaji.includes(test) || GlobalItems?.gameCards[index].onyomi.includes(test) || GlobalItems?.gameCards[index].kunyomi.includes(test)) {

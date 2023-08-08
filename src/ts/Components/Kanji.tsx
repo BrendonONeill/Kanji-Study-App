2e2ea7a6a3bdd192ea63da4feb7daf73cf6b1358
Brendon O'Neill <35762480+BrendonONeill@users.noreply.github.com>
2023-08-08 09:53:58 +0100

    Update Kanji.tsx

--- src/ts/Components/Kanji.tsx
+++ src/ts/Components/Kanji.tsx
@@ -6,7 +6,7 @@ const Kanji = () => {
   const GlobalItems = useContext(GlobalContext);
 
   const handleKeyPress = (event: any, index: number) => {
-    if (event.which === 13) {
+    if (event.which === 13 || event.which === 9) {
       let inputWord = event.target.value.trim();
       let test = inputWord.toLowerCase();
       if (GlobalItems?.gameCards[index].romaji.includes(test) || GlobalItems?.gameCards[index].onyomi.includes(test) || GlobalItems?.gameCards[index].kunyomi.includes(test)) {

882fe487403fae5d52f0e711065dab51b1886379
Brendon O'Neill <35762480+BrendonONeill@users.noreply.github.com>
2025-04-15 15:58:45 +0100

    Update fetch.ts

--- src/ts/Lib/fetch.ts
+++ src/ts/Lib/fetch.ts
@@ -1,6 +1,6 @@
 
 export async function fetchKanji() {
-    let res = await fetch(`https://yko8kw06tyklhyhrgtrg.brendon-projects.blog/kanji`);
+    let res = await fetch(`https://q8gs8gw0s8wwg0804wcw8ogo.brendons-projects.online/kanji`);
     let data = await res.json();
     return data.record;
 }

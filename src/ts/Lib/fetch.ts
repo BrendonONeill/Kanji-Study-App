981bfac915957cd3941e623aefc87bd9b584339c
Brendon O'Neill <brendonpjon@gmail.com>
2026-04-13 14:15:31 +0100

    new server

--- src/ts/Lib/fetch.ts
+++ src/ts/Lib/fetch.ts
@@ -1,5 +1,5 @@
 export async function fetchKanji() {
-    let res = await fetch(`https://q8gs8gw0s8wwg0804wcw8ogo.brendons-projects.online/kanji`);
+    let res = await fetch(`https://prox.ko-do.dev/kanji`);
     let data = await res.json();
     return data.record;
 }
